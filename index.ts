5eb209c24f9b628b98747905b6b985c4781ae8c3
saurabh chauhan <sourabh1202@gmail.com>
2022-12-24 09:34:37 +0200

    removed sequelize

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import express, { Express,Request,Response } from "express";
 import * as env from "dotenv"
 
+
 env.config()
 const app: Express = express();
 
 const PORT = process.env.PORT
 
-app.listen(PORT,()=>{
-    console.log("app started")
-})
+app.listen(PORT)
