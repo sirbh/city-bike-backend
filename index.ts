087e3ddc35678707c8c8d021680707f63c0ac693
saurabh chauhan <sourabh1202@gmail.com>
2022-12-26 20:10:33 +0200

    added new journey route

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express, { Express,Request,Response } from "express";
 import * as env from "dotenv"
-
+import cors from 'cors'
+import journeyRoutes from './router/journey'
 
 env.config()
 const app: Express = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(journeyRoutes)
 
 const PORT = process.env.PORT
 
