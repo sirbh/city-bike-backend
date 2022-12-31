81585d614a9cc4fe072e79d395bbf0cbfbf81032
saurabh chauhan <sourabh1202@gmail.com>
2022-12-31 21:22:38 +0200

    fixed seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,144 +1,56 @@
+import fs from "fs";
+import { join } from "path";
 import db from "../utils/database";
 
-import * as fs from "fs";
-import * as path from "path";
-import { parse, Parser } from "csv-parse";
+async function seed() {
+  const stationsDetailsPath = join(process.cwd(), "./files/station-details");
+  const stationDetailsFiles = await fs.readdirSync(stationsDetailsPath);
 
-type stationDetail = {
-  FID: number;
-  ID: number;
-  Nimi: string;
-  Namn: string;
-  Name: string;
-  Osoite: string;
-  Adress: string;
-  Kaupunki: string;
-  Stad: string;
-  Operaattor: string;
-  Kapasiteet: string;
-  x: number;
-  y: number;
-};
+  const journeyDetailsPath = join(process.cwd(), "./files/journey-details");
+  const journeyDetailsFiles = await fs.readdirSync(journeyDetailsPath);
 
-type journey = {
-    Departure:Date,
-    Return:Date,
-    "Departure station id":number,
-    "Departure station name":string,
-    "Return station id":number,
-    "Return station name":string,
-    "Covered distance (m)":number,
-    "Duration (sec.)":number
+  for (const file of stationDetailsFiles) {
+    await db.$queryRawUnsafe(
+      `COPY "stationDetails" FROM '${
+        stationsDetailsPath + "/" + file
+      }' csv header`
+    );
+  }
 
+  for (const file of journeyDetailsFiles) {
+    await db.$queryRawUnsafe(
+      `COPY "journey" ("departure","return","departure_station_id","departure_station_name","return_station_id","return_station_name","covered_distance","duration") FROM '${
+        journeyDetailsPath + "/" + file
+      }' csv header`
+    );
+    console.log(file);
+  }
+
+  const rec = await db.journey.deleteMany({
+    where: {
+      OR: [
+        {
+          covered_distance: {
+            lt: 10,
+          },
+        },
+        { covered_distance: null },
+        {duration:{
+          lt:10
+        }}
+      ],
+    },
+  });
+
+  console.log(rec);
 }
 
-const csvFilePath = path.resolve("C:/Users/soura/Downloads/stations.csv");
-const csvFilePath2 = path.resolve("C:/Users/soura/Downloads/2021-05.csv");
-
-const headers = [
-  "FID",
-  "ID",
-  "Nimi",
-  "Namn",
-  "Name",
-  "Osoite",
-  "Adress",
-  "Kaupunki",
-  "Stad",
-  "Operaattor",
-  "Kapasiteet",
-  "x",
-  "y",
-];
-
-const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
-const fileContent2 = fs.readFileSync(csvFilePath2, { encoding: "utf-8" });
-
-parse(
-  fileContent,
-  {
-    delimiter: ",",
-    columns: headers,
-    from: 2,
-    cast: (cv, context) => {
-      if (context.column === "FID" || context.column === "ID") {
-        return parseInt(cv);
-      }
-      if (context.column === "x" || context.column === "y") {
-        return parseFloat(cv);
-      }
-      return cv.trim() ? cv : undefined;
-    },
-  },
-  async (error, result: stationDetail[]) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log("here", result);
-    result.forEach(async (record) => {
-      await db.stationDetails.create({
-        data: {
-          adress: record.Adress,
-          id: record.FID,
-          Kapasiteet: record.Kapasiteet,
-          name: record.Name,
-          namn: record.Namn,
-          nimi: record.Nimi,
-          osoite: record.Osoite,
-          station_id: record.ID,
-          x: record.x,
-          y: record.y,
-          kaupunki: record.Kaupunki,
-          operaattor: record.Operaattor,
-          stad: record.Stad,
-        },
-      });
-    });
+async function main() {
+  try {
+    await seed();
+  } catch (e) {
+    console.log(e);
   }
-);
+}
 
-// parse(
-//   fileContent2,
-//   {
-//     delimiter: ",",
-//     from: 2,
-//   },
-//   async (error, result:journey[]) => {
-//     if (error) {
-//       console.error(error);
-//     }
-    
-//     result.forEach(async data=>{
-//        await db.journey.create({data:{
-//         departure:data.Departure,
-//         departure_station_id:data["Departure station id"],
-//         departure_station_name:data["Departure station name"],
-//         duration:data["Duration (sec.)"],
-//         return:data.Return,
-//         return_station_id:data["Return station id"],
-//         return_station_name:data["Return station name"],
-//         covered_distance:data["Covered distance (m)"],
-//        }})
-//     })
-//   }
-// ).on('readable', async function(this:Parser){
-//      let count = 0
-//      while(count<=103){
-//         const record = this.read()
-//         await db.journey.create({
-//           data:{
-//            departure:new Date(record[0]),
-//            return:new Date(record[1]),
-//            departure_station_id:parseInt(record[2]),
-//            departure_station_name:record[3],
-//            return_station_id:parseInt(record[4]),
-//            return_station_name:record[5],
-//            covered_distance:parseFloat(record[6]),
-//            duration:parseInt(record[7]) 
-//           }
-//         })
-//         count++
-//      }
-    
-//   })
-
+main();
