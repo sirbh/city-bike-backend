a2e3d7007c96863919cd624afbbda32ed86436e2
saurabh chauhan <sourabh1202@gmail.com>
2023-01-05 06:30:17 +0200

    fixed seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,28 +3,25 @@ import { join } from "path";
 import db from "../utils/database";
 
 async function seed() {
-
   const stationsDetailsPath = join("./files/station-details");
   const stationDetailsFiles = fs.readdirSync(stationsDetailsPath);
 
   const journeyDetailsPath = join("./files/journey-details");
   const journeyDetailsFiles = fs.readdirSync(journeyDetailsPath);
 
+  for (const file of stationDetailsFiles) {
+    await db.$queryRawUnsafe(
+      `COPY "stationDetails" FROM '../../../../${join(stationsDetailsPath.replace('.',''),file)}' csv header`
+    );
+    console.log("seeded " + file);
+  }
 
-  // for (const file of stationDetailsFiles) {
-  //   console.log(file)
-  //   const out = await db.$queryRawUnsafe(
-  //     `\copy stationDetails from '${join(stationsDetailsPath,file)}' csv header`
-  //   );
-
-  // }
-
-  // for (const file of journeyDetailsFiles) {
-  //   await db.$queryRawUnsafe(
-  //     `COPY "journey" ("departure","return","departure_station_id","departure_station_name","return_station_id","return_station_name","covered_distance","duration") from '/files/journey-details/2021-05.csv' csv header`
-  //   );
-  //   console.log(file);
-  // }
+  for (const file of journeyDetailsFiles) {
+    await db.$queryRawUnsafe(
+      `COPY "journey" ("departure","return","departure_station_id","departure_station_name","return_station_id","return_station_name","covered_distance","duration") from '../../../../${join(journeyDetailsPath.replace('.',''),file)}' csv header`
+    );
+    console.log("seeded " + file);
+  }
 
   const rec = await db.journey.deleteMany({
     where: {
